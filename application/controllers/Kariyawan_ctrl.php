622306db37d2bd44e7a9332ce78f22c5a6c2d02f
I-Shen <mihsannugi123@gmail.com>
2021-11-05 21:39:37 +0700

    Loker

---
+++ application/controllers/Kariyawan_ctrl.php
@@ -0,0 +1,68 @@
+<?php
+class Kariyawan_ctrl extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('model_kariyawan');
+	}
+	public function index()
+	{
+		# code...
+		$this->load->view('templates/Header_LG');
+		$this->load->view('Login_body');
+		$this->load->view('templates/Footer_LG');
+	}
+	public function registrasi()
+	{
+		# code...
+		$this->form_validation->set_rules('user_name', 'User name', 'trim|required', ['required' => 'User name harus di isi']);
+		$this->form_validation->set_rules(
+			'email',
+			'email',
+			'required|trim|valid_email|is_unique[registrasi.email]',
+			[
+				'required' => 'email harus di isi',
+				'is_unique' => 'email sudah terdaftar'
+			]
+		);
+		$this->form_validation->set_rules('password1', 'password', 'required|trim|min_length[8]|matches[password2]', [
+			'required' => 'password harus di isi',
+			'matches' => 'password tidak cocok!',
+			'min_length' => 'PASSWORD TERLALU LEMAH'
+		]);
+		$this->form_validation->set_rules('password2', 'password', 'required|trim|matches[password1]', ['required' => 'password harus di isi']);
+		$this->form_validation->set_rules('no_telp', 'phone number', 'required|trim', ['required' => 'nomer telepon harus di isi']);
+		$this->form_validation->set_rules('gender', 'gender', 'required|trim', ['required' => 'gender harus di pilih']);
+		$this->form_validation->set_rules('umur', 'umur', 'required|trim', ['required' => 'umur harus di isi']);
+
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('templates/Header_LG');
+			$this->load->view('Login_body');
+			$this->load->view('templates/Footer_LG');
+		} else {
+			$user = $this->input->post('user_name', true);
+			$email = $this->input->post('email', true);
+			$password = $this->input->post('password1');
+			$no_telp = $this->input->post('no_telp');
+			$gender = $this->input->post('gender');
+			$umur = $this->input->post('umur');
+			$this->model_kariyawan->insert_registrasi($user, $email, $password, $no_telp, $gender, $umur);
+			redirect(base_url('index.php/Kariyawan_ctrl'));
+		}
+	}
+	public function login()
+	{
+		# code...
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+		$registrasi = $this->model_kariyawan->cek_email($email, $password);
+	}
+	public function upload_cv()
+	{
+		$this->load->view('templates/Header');
+		$this->load->view('sidebar/Sidebar');
+		$this->load->view('Upload_body');
+		$this->load->view('templates/Footer');
+	}
+}
